1d212e6f608b6cd1a8ff5bf1d0a3e5219c028035
victor braz <victor.90braz@gmail.com>
2023-11-02 18:12:25 +0100

    $table->unsignedInteger('experience_points')->default(0);

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedInteger('experience_points')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
